01047e49708f3357e799643c58eac62c577adc39
whamer100 <whammer100@aol.com>
2019-06-23 16:54:52 -0700

    finalized switch to typescript

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,9 @@ const trHandler = (user: string) => {
   return getTyperacerStats(user)
 }
 
-app.get('/', (res: { send: (arg0: string) => void; }) => res.send(''))
-app.get('/stats', (res: { send: (arg0: string) => void; }) => res.send("Error: no stat page requested"))
-app.get('/stats/:page', async (req: { params: { [x: string]: string; }; }, res: { send: (arg0: string | { discord: any; reddit: any; }) => void; }) => res.send(await pageHandler(req.params["page"])))
-app.get('/tr/:user', async (req: { params: { [x: string]: any; }; }, res: { send: (arg0: any) => void; }) => res.send(await trHandler(req.params["user"])))
+app.get('/', (req, res) => res.send(''))
+app.get('/stats', (req, res) => res.send("Error: no stat page requested"))
+app.get('/stats/:page', async (req, res) => res.send(await pageHandler(req.params["page"])))
+app.get('/tr/:user', async (req, res) => res.send(await trHandler(req.params["user"])))
 
 app.listen(port, () => console.log(`listening on port ${port}`))
